f5bbd15692c44daed566e2d90c9daae9ffcf6e51
dwnorm2 <dwnorm2@gmail.com>
2023-10-03 14:23:21 -0500

    solve 6

--- main.js
+++ main.js
@@ -17,3 +17,71 @@ function binToDec(bin) {
 function binToDec(bin) {
   return parseInt(bin, 2);
 }
+
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// For example:
+
+// time = 3 ----> litres = 1
+
+// time = 6.7---> litres = 3
+
+// time = 11.8--> litres = 5
+
+let litres = (time) => Math.floor(time * 0.5);
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+let booleanToString = (b) => b.toString();
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`;
+}
+
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+function basicOp(operation, value1, value2) {
+  return eval(value1 + operation + value2);
+}
+
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+// Note: this kata uses strict construction as shown in the description and the examples, you can read more about it here
+
+let century = (year) => Math.ceil(year / 100);
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+let maps = (x) => x.map((y) => y * 2);
