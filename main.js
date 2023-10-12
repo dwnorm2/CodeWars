208a9309700e39af88f047835d32c27d84425de8
dwnorm2 <dwnorm2@gmail.com>
2023-10-12 15:27:42 -0500

    1 8kyu

--- main.js
+++ main.js
@@ -524,3 +524,15 @@ const areaOrPerimeter = function (l, w) {
 function setAlarm(employed, vacation) {
   return employed && !vacation ? true : false;
 }
+
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+function sumMix(x) {
+  let sum = 0;
+  for (let num of x) {
+    sum += Number(num);
+  }
+  return sum;
+}
