b894e79882bfa07537613ca0b206d29d8c676a5c
dwnorm2 <dwnorm2@gmail.com>
2023-10-14 16:29:08 -0500

    1 8kyu

--- main.js
+++ main.js
@@ -554,3 +554,42 @@ function sumMix(x) {
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
+
+// Task
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+// Mind the input validation.
+
+// Example
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+// Input validation
+// If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+function sumArray(array) {
+  if (array == null || array.length < 2) {
+    return 0;
+  } else {
+    let lowestNum = array[0];
+    let highestNum = array[0];
+    let newArr = [];
+    for (let num of array) {
+      if (num > lowestNum) {
+        lowestNum = num;
+      }
+      if (num < highestNum) {
+        highestNum = num;
+      }
+      newArr.push(num);
+    }
+    console.log(newArr);
+
+    let sum = 0;
+    for (let num of newArr) {
+      sum += num;
+    }
+    return sum - lowestNum - highestNum;
+  }
+}
