268cabe58cf7f7a900ec3000f883a4d76ae14c75
dwnorm2 <dwnorm2@gmail.com>
2023-10-16 12:28:45 -0500

    1 8kyu

--- main.js
+++ main.js
@@ -622,3 +622,14 @@ function feast(beast, dish) {
 // Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
 
 let getAge = (inputString) => parseInt(inputString);
+
+// The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+// For example:
+
+// 1.08 --> 30
+// Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+
+function cockroachSpeed(s) {
+  return Math.floor(s / 0.036);
+}
