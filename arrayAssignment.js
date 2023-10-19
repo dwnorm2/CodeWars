f7ebe686fe4baa208a1be9e25a0acb128232bbef
dwnorm2 <dwnorm2@gmail.com>
2023-10-19 18:14:10 -0500

    add array assignments

--- arrayAssignment.js
+++ arrayAssignment.js
@@ -16,3 +16,33 @@ function camelize(str) {
 }
 
 camelize("-webkit-transition");
+
+//Filter range
+
+function filterRange(arr, a, b) {
+  return arr.filter((item) => a <= item && item <= b);
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+filterRange(arr, 1, 4);
+
+//Filter range "in place"
+
+function filterRangeInPlace(arr, a, b) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < a || arr[i] > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+filterRangeInPlace(arr, 1, 3);
+console.log(arr);
+
+// Sort in decreasing order
+
+let arr3 = [5, 2, 1, -10, 8];
+
+arr3.sort((n1, n2) => n2 - n1);
+console.log(arr3);
