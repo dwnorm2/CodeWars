f6e23f7d2c8e3a288fd4962ab8c7844fb180b5ec
dwnorm2 <dwnorm2@gmail.com>
2023-10-21 16:48:20 -0500

    1 8kyu

--- main.js
+++ main.js
@@ -928,3 +928,19 @@ function strCount(str, letter) {
 function testEven(n) {
   return n % 2 == 0;
 }
+
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+
+function between(a, b) {
+  let arr = [];
+  for (let i = a; a <= b; a++) {
+    arr.push(a);
+  }
+  return arr;
+}
