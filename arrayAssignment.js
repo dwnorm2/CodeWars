6c06320e06433c31411aaa082c3c3817004c32a6
dwnorm2 <dwnorm2@gmail.com>
2023-10-21 18:03:33 -0500

    finish array assignment

--- arrayAssignment.js
+++ arrayAssignment.js
@@ -59,3 +59,121 @@ function copySorted(arr) {
 
 console.log(sorted);
 console.log(arr4);
+
+// Create an extendable calculator
+
+let calc = new Calculator();
+
+function Calculator() {
+  this.methods = {
+    "-": (a, b) => a - b,
+    "+": (a, b) => a + b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => a / b,
+    "**": (a, b) => a ** b,
+  };
+
+  this.calculate = function (str) {
+    let split = str.split(" "),
+      a = +split[0],
+      op = split[1],
+      b = +split[2];
+
+    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+      return NaN;
+    }
+
+    return this.methods[op](a, b);
+  };
+
+  this.addMethod = function (name, func) {
+    this.methods[name] = func;
+  };
+}
+
+//Map to names
+
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [john, pete, mary];
+
+let names = users.map((user) => user.name);
+names;
+
+//Map to objects
+
+let usersMapped = users.map((user) => ({
+  fullName: `${user.name} ${user.surname}`,
+  id: user.id,
+}));
+usersMapped;
+
+//Sort users by age
+
+let john2 = { name: "John", age: 25 };
+let pete2 = { name: "Pete", age: 30 };
+let mary2 = { name: "Mary", age: 28 };
+
+let arr5 = [pete2, john2, mary2];
+arr5;
+
+function sortByAge(arr) {
+  arr.sort((a, b) => a.age - b.age);
+}
+
+sortByAge(arr5);
+arr5;
+
+//Shuffle an array
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+let arr6 = [1, 2, 3, 4, 5, 6, 7];
+shuffle(arr6);
+console.log(arr6);
+
+//Get average age
+function getAverageAge(users) {
+  return users.reduce((prev, user) => prev + user.age, 0 / users.length);
+}
+
+//Filter unique array members
+
+function unique(arr) {
+  let result = [];
+
+  for (let str of arr) {
+    if (!result.includes(str)) {
+      result.push(str);
+    }
+  }
+
+  return result;
+}
+
+let strings = [
+  "Hare",
+  "Krishna",
+  "Hare",
+  "Krishna",
+  "Krishna",
+  "Krishna",
+  "Hare",
+  "Hare",
+  ":-O",
+];
+
+alert(unique(strings)); // Hare, Krishna, :-O
+
+//Create keyed object from array
+
+function groupById(array) {
+  return array.reduce((obj, value) => {
+    obj[value.id] = value;
+    return obj;
+  }, {});
+}
