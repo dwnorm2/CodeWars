3418a40633f943ac30f3f0d6f9c28e8ea2650da3
dwnorm2 <dwnorm2@gmail.com>
2023-10-23 17:20:22 -0500

    1 8kyu

--- main.js
+++ main.js
@@ -944,3 +944,24 @@ function between(a, b) {
   }
   return arr;
 }
+
+// altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+// "hello world".toAlternatingCase() === "HELLO WORLD"
+// "HELLO WORLD".toAlternatingCase() === "hello world"
+// "hello WORLD".toAlternatingCase() === "HELLO world"
+// "HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+// "12345".toAlternatingCase()       === "12345"                   // Non-alphabetical characters are unaffected
+// "1a2b3c4d5e".toAlternatingCase()  === "1A2B3C4D5E"
+// "String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+// As usual, your function/method should be pure, i.e. it should not mutate the original string.
+
+String.prototype.toAlternatingCase = function () {
+  let arr = this.split("");
+  let altArr = [];
+  altArr = arr.map((x) =>
+    x.toUpperCase() == x ? x.toLowerCase() : x.toUpperCase()
+  );
+  return altArr.join("");
+};
