56157e86d2325929a3551af1e43081ab5a708df4
dwnorm2 <dwnorm2@gmail.com>
2023-10-27 11:45:58 -0500

    2 8kyu

--- main.js
+++ main.js
@@ -1282,3 +1282,39 @@ function greet(language) {
   }
   return data[1];
 }
+
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// Examples (Input -> Output)
+// * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// * [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+function reverseList(list) {
+  return list.reverse();
+}
+
+// Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+// "I love you"
+// "a little"
+// "a lot"
+// "passionately"
+// "madly"
+// "not at all"
+// If there are more than 6 petals, you start over with "I love you" for 7 petals, "a little" for 8 petals and so on.
+
+// When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+// Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
+
+function howMuchILoveYou(nbPetals) {
+  let choices = [
+    "I love you",
+    "a little",
+    "a lot",
+    "passionately",
+    "madly",
+    "not at all",
+  ];
+  return choices[(nbPetals - 1) % 6];
+}
