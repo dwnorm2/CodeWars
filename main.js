23fc4c3bc8a40b3606151ea10f0b3af2b5044597
dwnorm2 <dwnorm2@gmail.com>
2023-10-29 12:05:53 -0500

    1 8kyu

--- main.js
+++ main.js
@@ -1401,3 +1401,36 @@ function gooseFilter(birds) {
 
   return birds.filter((x) => !geese.includes(x));
 }
+
+// Americans are odd people: in their buildings, the first floor is actually the ground floor and there is no 13th floor (due to superstition).
+
+// Write a function that given a floor in the american system returns the floor in the european system.
+
+// With the 1st floor being replaced by the ground floor and the 13th floor being removed, the numbers move down to take their place. In case of above 13, they move down by two because there are two omitted numbers below them.
+
+// Basements (negatives) stay the same as the universal level.
+
+// More information here
+
+// Examples
+// 1  =>  0
+// 0  =>  0
+// 5  =>  4
+// 15  =>  13
+// -3  =>  -3
+
+function getRealFloor(n) {
+  return n >= 13 ? n - 2 : n > 0 ? n - 1 : n;
+}
+
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// 65 --> 'A'
+// 97 --> 'a'
+// 48 --> '0
+
+function getChar(n) {
+  return String.fromCharCode(n);
+}
