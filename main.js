b4602e970b567a5041fa0163a551034e752fc4f8
dwnorm2 <dwnorm2@gmail.com>
2023-10-29 11:30:13 -0500

    1 8kyu

--- main.js
+++ main.js
@@ -1338,3 +1338,17 @@ function findMultiples(integer, limit) {
   }
   return arr;
 }
+
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+// Examples
+// "hello"     -->  "hll"
+// "codewars"  -->  "cdwrs"
+// "goodbye"   -->  "gdby"
+// "HELLO"     -->  "HELLO"
+// don't worry about uppercase vowels
+// y is not considered a vowel for this kata
+
+function shortcut(string) {
+  return string.replaceAll(/a|e|i|o|u/g, "");
+}
