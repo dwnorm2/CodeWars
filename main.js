df77148acd46359394598037ce215544d8348a3c
dwnorm2 <dwnorm2@gmail.com>
2023-11-01 12:19:21 -0500

    more 7kyu

--- main.js
+++ main.js
@@ -1504,3 +1504,37 @@ function getCount(str) {
       0
     );
 }
+
+// Trolls are attacking your comment section!
+
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+// Note: for this kata y isn't considered a vowel.
+
+function disemvowel(str) {
+  return str.replace(/a|e|i|o|u|A|E|I|O|U/g, "");
+}
+
+//or
+
+function disemvowel(str) {
+  return str.replace(/[aeiou]/gi, "");
+}
+
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+// Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+// Note: The function accepts an integer and returns an integer.
+// Happy Coding!
+
+function squareDigits(num) {
+  return +num
+    .toString()
+    .split("")
+    .map((num) => num ** 2)
+    .join("");
+}
