2590385f302225045adb681298991021acb5af47
dwnorm2 <dwnorm2@gmail.com>
2023-11-03 15:05:33 -0500

    1 kata

--- main.js
+++ main.js
@@ -1644,3 +1644,20 @@ function isSquare(n) {
 function saleHotdogs(n) {
   return n < 5 ? 100 * n : n >= 5 && n < 10 ? 95 * n : 90 * n;
 }
+
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+function descendingOrder(n) {
+  return +n
+    .toString()
+    .split("")
+    .sort((a, b) => b - a)
+    .join("");
+}
