3d2e93ce5e729fa621337cee4fdb052c1f9352f8
dwnorm2 <dwnorm2@gmail.com>
2023-11-06 11:35:28 -0600

    1 kata

--- main.js
+++ main.js
@@ -1767,3 +1767,25 @@ String.prototype.toJadenCase = function () {
 // String will never be empty and you do not need to account for different data types.
 
 let findShort = (s) => Math.min(...s.split(" ").map((x) => x.length));
+
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+  let arr = [];
+  for (let i = 0; i < s.length; i++) {
+    for (let x = 0; x <= i; x++) {
+      if (x === 0) {
+        arr.push(s[i].toUpperCase());
+      } else arr.push(s[i].toLowerCase());
+    }
+    arr.push("-");
+  }
+  arr.pop();
+  return arr.join("");
+}
