62581b9103c81f14f26cd89402408a2cedb19fbc
Dan <dwnorm2@gmail.com>
2023-11-11 14:35:53 -0600

    1 kata

--- main.js
+++ main.js
@@ -1925,3 +1925,17 @@ function mergeArrays(a, b) {
     .map((x) => Number(x))
     .filter((e, i, a) => e !== a[i - 1]);
 }
+
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// Examples:
+
+// Input -> Output
+// [1, 1, 2] -> [1, 2]
+// [1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+function distinct(a) {
+  return a.filter((x,i) => a.indexOf(x) === i);
+}
