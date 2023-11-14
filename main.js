ea18fd5d67a86b872c1407a1e79d7f2ce737e08c
Dan <dwnorm2@gmail.com>
2023-11-14 11:00:28 -0600

    1 kata

--- main.js
+++ main.js
@@ -1966,3 +1966,8 @@ function mergeArrays(arr1, arr2) {
 function unusualFive() {
   return ["a","a","a","a","a"].length
   }
+
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+let problem = x => typeof x == "number" ? x * 50 + 6 : "Error"
+  
