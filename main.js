27eea1ccc24387b1ecbcb9eb015fd245c4f69f41
Dan <dwnorm2@gmail.com>
2023-11-14 11:21:02 -0600

    1 kata

--- main.js
+++ main.js
@@ -1970,4 +1970,26 @@ function unusualFive() {
 // Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
 
 let problem = x => typeof x == "number" ? x * 50 + 6 : "Error"
-  
+
+// Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+
+// Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
+
+// [sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+//    7      6      5      4      3            2      1
+
+// If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+
+// Note: there will always be exactly one wolf in the array.
+// Examples
+
+// Input: ["sheep", "sheep", "sheep", "wolf", "sheep"]
+// Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+
+// Input: ["sheep", "sheep", "wolf"]
+// Output: "Pls go away and stop eating my sheep"
+
+function warnTheSheep(queue) {
+  let i = queue.reverse().indexOf("wolf")
+  return i > 0 ? `Oi! Sheep number ${i}! You are about to be eaten by a wolf!` : "Pls go away and stop eating my sheep"
+  }
