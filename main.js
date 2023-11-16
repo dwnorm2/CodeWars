2f1ad7920c005f64bf58d8ed0b30b4425e592aec
Dan <dwnorm2@gmail.com>
2023-11-15 21:02:39 -0600

    revert ro original

--- main.js
+++ main.js
@@ -2010,5 +2010,5 @@ function warnTheSheep(queue) {
 function chromosomeCheck(sperm) {
   return sperm.includes("Y")
     ? "Congratulations! You're going to have a son."
-    : "Congratulations! You're going to have a daughter?";
+    : "Congratulations! You're going to have a daughter.";
 }
