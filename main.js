3b91847de5163979eead914530a211b4ce53d8c2
Dan <dwnorm2@gmail.com>
2023-11-17 12:53:24 -0600

    1 kata

--- main.js
+++ main.js
@@ -2044,3 +2044,15 @@ let toBinary = n => +n.toString(2)
 function arr2bin(arr){
   return arr.reduce((sum,num) =>  sum + (typeof num == "number" ?  num : 0) , 0).toString(2)
 }
+
+// The wide-mouth frog is particularly interested in the eating habits of other creatures.
+
+// He just can't stop asking the creatures he encounters what they like to eat. But, then he meets the alligator who just LOVES to eat wide-mouthed frogs!
+
+// When he meets the alligator, it then makes a tiny mouth.
+
+// Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide.
+
+function mouthSize(animal) {
+  return animal.toLowerCase() == "alligator" ? "small" : "wide"
+}
