0d3d87207477445b94a0caa91c290d3046f6de4d
Dan <dwnorm2@gmail.com>
2023-11-18 18:53:17 -0600

    1 kata

--- main.js
+++ main.js
@@ -2056,3 +2056,19 @@ function arr2bin(arr){
 function mouthSize(animal) {
   return animal.toLowerCase() == "alligator" ? "small" : "wide"
 }
+
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+function well(x){
+  let goods = []
+  for (let y of x) {
+    if (y == "good") {
+      goods.push(y)
+    } 
+  }
+  return goods.length > 2 ? "I smell a series!"
+    : goods.length > 0 ? "Publish!"
+    : "Fail!"
+  }
