8045c4f7b5f22d9c7364835320b4097278c156f4
Dan <dwnorm2@gmail.com>
2023-11-23 12:45:23 -0600

    1 kata

--- main.js
+++ main.js
@@ -2157,3 +2157,42 @@ var findAverage = function (nums) {
 function isDigit(s) {
   return parseFloat(s) == s;
 }
+
+function sumMul(n, m) {
+  let sum = 0;
+  if (m > 0 && n > 0) {
+    for (let i = 1; i < m / n; i++) {
+      sum += i * n;
+    }
+    return sum;
+  } else {
+    return 'INVALID';
+  }
+}
+
+// Your Job
+
+// Find the sum of all multiples of n below m
+// Keep in Mind
+
+//     n and m are natural numbers (positive integers)
+//     m is excluded from the multiples
+
+// Examples
+
+// sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+// sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+// sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+// sumMul(4, -7)  ==> "INVALID"
+
+function sumMul(n, m) {
+  let sum = 0;
+  if (m > 0 && n > 0) {
+    for (let i = 1; i < m / n; i++) {
+      sum += i * n;
+    }
+    return sum;
+  } else {
+    return 'INVALID';
+  }
+}
