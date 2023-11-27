c88b816aaf103de35c5e4fd4425286f5ad236294
Dan <dwnorm2@gmail.com>
2023-11-27 16:45:25 -0600

    1 kata

--- main.js
+++ main.js
@@ -2282,3 +2282,14 @@ function datingRange(age) {
     ? `${Math.floor(age - 0.1 * age)}-${Math.floor(age + 0.1 * age)}`
     : `${Math.floor(age / 2 + 7)}-${Math.floor((age - 7) * 2)}`;
 }
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+function solution(str, ending) {
+  return str.endsWith(ending);
+}
