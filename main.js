eb12c1b746ac31fd591cdf2c6b7bfd7791f0324c
Dan <dwnorm2@gmail.com>
2023-11-30 15:16:25 -0600

    1 kata

--- main.js
+++ main.js
@@ -2327,3 +2327,9 @@ function getSum(a, b) {
 // 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
 
 let addBinary = (a, b) => (a + b).toString(2);
+
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+const isTriangle = (a, b, c) => a + b > c && a + c > b && b + c > a;
