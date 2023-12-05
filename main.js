5ccea69847cb9193c19886c016302b1f25c0b540
Dan <dwnorm2@gmail.com>
2023-12-05 14:47:03 -0600

    1 kata

--- main.js
+++ main.js
@@ -2394,3 +2394,10 @@ function noBoringZeros(n) {
   }
   return n;
 }
+
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+function combat(health, damage) {
+  let newHealth = health - damage;
+  return newHealth >= 0 ? newHealth : 0;
+}
