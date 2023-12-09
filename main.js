6ab95f8f0ed87b74192091832d7dd96ed762601f
Dan <dwnorm2@gmail.com>
2023-12-09 12:32:16 -0600

    1 kata

--- main.js
+++ main.js
@@ -2428,3 +2428,13 @@ function take(arr, n) {
 function sayHello(name, city, state) {
   return `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`;
 }
+
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+function longest(s1, s2) {
+  let arr = [...s1, ...s2];
+  return arr
+    .sort()
+    .filter((x, i) => arr[i] !== arr[i - 1])
+    .join("");
+}
