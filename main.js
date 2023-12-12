37cd68b833fc26ee1c0035097a37397557f90161
Dan <dwnorm2@gmail.com>
2023-12-12 13:56:50 -0600

    1 kata

--- main.js
+++ main.js
@@ -2514,3 +2514,18 @@ function nbYear(p0, percent, aug, p) {
 function openOrSenior(data) {
   return data.map((app) => (app[0] >= 55 && app[1] > 7 ? "Senior" : "Open"));
 }
+
+// You might know some pretty large perfect squares. But what about the NEXT one?
+
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+// Examples:(Input --> Output)
+
+// 121 --> 144
+// 625 --> 676
+// 114 --> -1 since 114 is not a perfect square
+
+let findNextSquare = (sq) =>
+  Math.sqrt(sq) % 1 ? -1 : Math.pow(Math.sqrt(sq) + 1, 2);
