e9085cf4b6c5c6a02cad2ba3848bc77ceabbb060
Dan <dwnorm2@gmail.com>
2023-12-23 12:49:21 -0600

    1 kata

--- main.js
+++ main.js
@@ -2711,3 +2711,20 @@ function removeSmallest(numbers) {
   let indexOfMin = numbers.indexOf(Math.min(...numbers));
   return numbers.filter((num, index) => index !== indexOfMin);
 }
+
+// Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+// For example: (Input --> Output)
+
+// 10 --> 1
+// 99 --> 18
+// -32 --> 5
+
+// Let's assume that all numbers in the input will be integer values.
+
+function sumDigits(number) {
+  return Math.abs(number)
+    .toString()
+    .split("")
+    .reduce((total, num) => total + Number(num), 0);
+}
