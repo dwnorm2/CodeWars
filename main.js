ed1727e821a734d19b5575a3904e1feeb088a34d
Dan <dwnorm2@gmail.com>
2023-12-25 15:27:41 -0600

    1 kata

--- main.js
+++ main.js
@@ -2740,3 +2740,18 @@ function sumDigits(number) {
 function spEng(sentence) {
   return sentence.toLowerCase().includes("english");
 }
+
+// Template Strings
+// Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+// Task
+// Your task is to return the correct string using the Template String Feature.
+// Input
+// Two Strings, no validation is needed.
+// Output
+// You must output a string containing the two strings with the word ```' are '```
+
+// Reference: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/template_strings
+
+var templateStrings = function (noun, adjective) {
+  return `${noun} are ${adjective}`;
+};
