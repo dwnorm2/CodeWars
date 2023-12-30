87d1c47a83f49dac8289be02a74391d510de455d
Dan <dwnorm2@gmail.com>
2023-12-30 12:54:20 -0600

    1 kata

--- main.js
+++ main.js
@@ -2781,3 +2781,24 @@ function solution(string) {
 function remainder(n, m) {
   return n > m ? n % m : m % n;
 }
+
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+function position(letter) {
+  return `Position of alphabet: ${letter.charCodeAt(0) - 96}`;
+}
+
+// You have an award-winning garden and every day the plants need exactly 40mm of water. You created a great piece of JavaScript to calculate the amount of water your plants will need when you have taken into consideration the amount of rain water that is forecast for the day. Your jealous neighbour hacked your computer and filled your code with bugs.
+
+// Your task is to debug the code before your plants die!
+
+function rainAmount(mm) {
+  if (mm < 40) {
+    return "You need to give your plant " + `${40 - mm}` + "mm of water";
+  }
+  return "Your plant has had more than enough water for today!";
+}
