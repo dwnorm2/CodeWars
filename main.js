909e4e15141fa804e38f5ff06ebb6953d2d95f3f
Dan <dwnorm2@gmail.com>
2024-01-03 11:33:08 -0600

    1 kata

--- main.js
+++ main.js
@@ -2894,3 +2894,18 @@ function apple(x) {
     ? "It's hotter than the sun!!"
     : "Help yourself to a honeycomb Yorkie for the glovebox.";
 }
+
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+var number = function (array) {
+  return array.map((elem, i) => `${i + 1}: ${elem}`);
+};
