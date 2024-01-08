c406de99127c7f1ff5fd6a9d9ced9291250a182f
Dan <dwnorm2@gmail.com>
2024-01-08 12:39:42 -0600

    1 kata

--- main.js
+++ main.js
@@ -2962,6 +2962,30 @@ function mango(quantity, price) {
 
 // Note2: the input data can be: boolean array, array of objects, array of string arrays, array of number arrays... 😕
 
-function printArray(array){
-  return array.join(",")
+function printArray(array) {
+  return array.join(",");
+}
+
+// Your friend has been out shopping for puppies (what a time to be alive!)... He arrives back with multiple dogs, and you simply do not know how to respond!
+
+// By repairing the function provided, you will find out exactly how you should respond, depending on the number of dogs he has.
+
+// The number of dogs will always be a number and there will always be at least 1 dog.
+
+// Good luck!
+
+function howManyDalmatians(number) {
+  var dogs = [
+    "Hardly any",
+    "More than a handful!",
+    "Woah that's a lot of dogs!",
+    "101 DALMATIANS!!!",
+  ];
+  return number <= 10
+    ? dogs[0]
+    : number <= 50
+    ? dogs[1]
+    : number == 101
+    ? dogs[3]
+    : dogs[2];
 }
