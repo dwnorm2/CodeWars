69991276566e484ed474a7987841d6ae8774c48f
Dan Norman <112597888+dwnorm2@users.noreply.github.com>
2024-01-11 12:46:17 -0600

    1 kata

--- main.js
+++ main.js
@@ -2989,3 +2989,18 @@ function howManyDalmatians(number) {
     ? dogs[3]
     : dogs[2];
 }
+
+/*
+Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+For example:
+
+solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+solution(null); // should return []
+*/
+
+function solution(nums){
+  return nums ? nums.sort((a,b) => a-b) 
+  : []
+}
+
