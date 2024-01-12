8ff259c4201aa2006f87fa26150fbe85eb6894e3
Dan <dwnorm2@gmail.com>
2024-01-12 17:00:06 -0600

    1 kata

--- main.js
+++ main.js
@@ -2999,8 +2999,29 @@ solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
 solution(null); // should return []
 */
 
-function solution(nums){
-  return nums ? nums.sort((a,b) => a-b) 
-  : []
+function solution(nums) {
+  return nums ? nums.sort((a, b) => a - b) : [];
 }
 
+// This is a spin off of my first kata.
+
+// You are given a string containing a sequence of character sequences separated by commas.
+
+// Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+// If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+// Examples
+
+// "1,2,3"      =>  "2"
+// "1,2,3,4"    =>  "2 3"
+// "1,2,3,4,5"  =>  "2 3 4"
+
+// ""     =>  NULL
+// "1"    =>  NULL
+// "1,2"  =>  NULL
+
+function array(string) {
+  return string.split(",").length > 2
+    ? string.split(",").slice(1, -1).join(" ")
+    : null;
+}
