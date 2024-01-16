d58f4de7024d6b38d7f3de8c976f0272ecdd5d27
Dan <dwnorm2@gmail.com>
2024-01-16 13:06:01 -0600

    1 kata

--- main.js
+++ main.js
@@ -3071,3 +3071,17 @@ function isItANum(str) {
 
   return nums.length == 11 && nums[0] == 0 ? nums : "Not a phone number";
 }
+
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+// Examples
+
+// [1, 1, 2] ==> 2
+// [17, 17, 3, 17, 17, 17, 17] ==> 3
+
+function stray(numbers) {
+  return +numbers.filter((x, i, arr) => arr.indexOf(x) == arr.lastIndexOf(x));
+}
