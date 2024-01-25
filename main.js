5eb5f9265b4261b3459fe084ffc5587e74be0de6
Dan <dwnorm2@gmail.com>
2024-01-25 17:06:33 -0600

    1 kata

--- main.js
+++ main.js
@@ -3195,3 +3195,26 @@ var name = a + b;
 function oddCount(n) {
   return Math.floor(n / 2);
 }
+
+// Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+// Examples (input --> output):
+
+// "4556364607935616" --> "############5616"
+//      "64607935616" -->      "#######5616"
+//                "1" -->                "1"
+//                 "" -->                 ""
+
+// // "What was the name of your first pet?"
+// "Skippy" --> "##ippy"
+// "Nananananananananananananananana Batman!" --> "####################################man!"
+
+function maskify(cc) {
+  return cc
+    .split("")
+    .slice(0, -4)
+    .map((num) => "#")
+    .concat(cc.split("").slice(-4))
+    .join("");
+}
