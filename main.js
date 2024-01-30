b119b9c534875efb7ed8d84c39e33e8f78ce6dbe
Dan <dwnorm2@gmail.com>
2024-01-30 13:57:26 -0600

    1 kata

--- main.js
+++ main.js
@@ -3235,3 +3235,14 @@ function divisors(integer) {
   }
   return divs.length > 0 ? divs : `${integer} is prime`;
 }
+
+// Your task is to split the chocolate bar of given dimension n x m into small squares. Each square is of size 1x1 and unbreakable. Implement a function that will return minimum number of breaks needed.
+
+// For example if you are given a chocolate bar of size 2 x 1 you can split it to single squares in just one break, but for size 3 x 1 you must do two breaks.
+
+// If input data is invalid you should return 0 (as in no breaks are needed if we do not have any chocolate to split). Input will always be a non-negative integer.
+
+function breakChocolate(n, m) {
+  let breaks = n * m - 1;
+  return breaks > 0 ? breaks : 0;
+}
