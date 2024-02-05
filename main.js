ff959181181f1c5f65d585d1c660740953ecd56e
Dan <dwnorm2@gmail.com>
2024-02-05 13:09:11 -0600

    6kyu array

--- main.js
+++ main.js
@@ -3495,3 +3495,29 @@ function fixTheMeerkat(arr) {
 function sortByLength(array) {
   return array.sort((a, b) => a.length - b.length);
 }
+
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+// Example
+
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+
+// The returned format must be correct in order to complete this challenge.
+
+// Don't forget the space after the closing parentheses!
+
+function createPhoneNumber(numbers) {
+  numbers.splice(0, 0, "(");
+  numbers.splice(4, 0, ")");
+  numbers.splice(5, 0, " ");
+  numbers.splice(9, 0, "-");
+  return numbers.join("");
+}
+
+function createPhoneNumber(numbers) {
+  let format = "(###) ###-####";
+
+  for (let i = 0; i < numbers.length; i++) {
+    format = format.replace("#", numbers[i]);
+  }
+  return format;
+}
