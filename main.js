0590a3029e2534d9207a59aa519622504683c34a
Dan <dwnorm2@gmail.com>
2024-02-11 16:32:59 -0600

    1 kata

--- main.js
+++ main.js
@@ -3662,3 +3662,21 @@ function factorial(n) {
 
   return product;
 }
+
+// Instructions
+
+// Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters (word) as its argument, and returns an ordered list containing the indices of all capital (uppercase) letters in the string.
+// Example (Input --> Output)
+
+// "CodEWaRs" --> [0,3,4,6]
+
+var capitals = function (word) {
+  return word
+    .split("")
+    .map((x, i, arr) => {
+      if (x == x.toUpperCase()) {
+        return i;
+      }
+    })
+    .filter((x) => x == +x);
+};
