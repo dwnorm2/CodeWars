6c62511767db3c0e9e48f55f40f535af02484ee1
Dan <dwnorm2@gmail.com>
2024-02-15 14:07:41 -0600

    1 kata

--- main.js
+++ main.js
@@ -3716,3 +3716,28 @@ function angle(n) {
 function gimme(triplet) {
   return triplet.indexOf([...triplet].sort((a, b) => a - b)[1]);
 }
+
+// Task
+
+// Given a string str, reverse it and omit all non-alphabetic characters.
+// Example
+
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+// Input/Output
+
+//     [input] string str
+
+// A string consists of lowercase latin letters, digits and symbols.
+
+//     [output] a string
+
+function reverseLetter(str) {
+  let letters = "abcdefghijklmnopqrstuvwxyz";
+  return str
+    .split("")
+    .filter((x) => letters.includes(x))
+    .reverse()
+    .join("");
+}
