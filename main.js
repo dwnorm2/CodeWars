423c9e591de73058eb4c89faaa35aaddf3146fce
Dan <dwnorm2@gmail.com>
2024-02-20 12:32:17 -0600

    1 kata

--- main.js
+++ main.js
@@ -3836,3 +3836,28 @@ function capitalize(s) {
       .join(""),
   ];
 }
+
+// In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+
+//     make as few changes as possible.
+//     if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+
+// For example:
+
+// solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+// solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+// solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+
+function solve(s) {
+  let upperCount = 0;
+  let lowerCount = 0;
+  let arr = s.split("");
+
+  for (let l of arr) {
+    l == l.toUpperCase() ? upperCount++ : lowerCount++;
+  }
+
+  return upperCount > lowerCount
+    ? arr.map((l) => l.toUpperCase()).join("")
+    : arr.map((l) => l.toLowerCase()).join("");
+}
