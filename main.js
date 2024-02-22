27659b46d9b46646202987dccce2d4a598cbd583
dwnorm2 <dwnorm2@gmail.com>
2024-02-22 13:10:44 -0600

    1 kata

--- main.js
+++ main.js
@@ -3876,3 +3876,24 @@ function consecutive(arr, a, b) {
   }
   return false;
 }
+
+// A non-empty array a of length n is called an array of all possibilities if it contains all numbers between [0,a.length-1].Write a method named isAllPossibilities that accepts an integer array and returns true if the array is an array of all possibilities, else false.
+
+// Example:
+
+// a=[1,2,0,3]
+// a.length-1=3
+// a includes [0,3] ,hence the function should return true
+
+function isAllPossibilities(x) {
+  let sorted = x.sort((a, b) => a - b);
+  for (let i = 0; i < sorted.length; i++) {
+    if (i !== sorted[i]) {
+      return false;
+    }
+  }
+  if (x.includes(0) && x.includes(x.length - 1)) {
+    return true;
+  }
+  return false;
+}
