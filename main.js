37ed6fddfb3ccd32371a834d4955d9acf0ea700e
dwnorm2 <dwnorm2@gmail.com>
2024-02-27 17:44:03 -0600

    1 kata

--- main.js
+++ main.js
@@ -4005,3 +4005,27 @@ function checkExam(array1, array2) {
 function digits(n) {
   return n.toString().length;
 }
+
+// Challenge:
+
+// Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+// Example:
+
+// Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+// Addendum:
+
+// Please, keep in mind, that JavaScript is by default sorting objects alphabetically. For more information, please consult:
+
+// http://stackoverflow.com/questions/6093874/why-doesnt-the-sort-function-of-javascript-work-well
+
+function flattenAndSort(array) {
+  let arr = [];
+  for (let arrays of array) {
+    for (let num of arrays) {
+      arr.push(num);
+    }
+  }
+  return arr.sort((a, b) => a - b);
+}
