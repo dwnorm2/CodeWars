9bfd033422fa5eb0c8627def5c54ad48758d39f6
dwnorm2 <dwnorm2@gmail.com>
2024-03-02 20:04:30 -0600

    1 kata

--- main.js
+++ main.js
@@ -4121,3 +4121,27 @@ function minValue(values) {
     .sort((a, b) => a - b)
     .join("");
 }
+
+// Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+// Replace certain values however if any of the following conditions are met:
+
+//     If the value is a multiple of 3: use the value "Fizz" instead
+//     If the value is a multiple of 5: use the value "Buzz" instead
+//     If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+
+// N will never be less than 1.
+
+// Method calling example:
+
+// fizzbuzz(3) -->  [1, 2, "Fizz"]
+
+function fizzbuzz(n) {
+  let arr = [];
+  for (let i = 1; i <= n; i++) {
+    arr.push(i);
+  }
+  return arr.map((n) =>
+    n % 15 == 0 ? "FizzBuzz" : n % 5 == 0 ? "Buzz" : n % 3 == 0 ? "Fizz" : n
+  );
+}
