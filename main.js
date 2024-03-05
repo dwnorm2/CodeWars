0f358e181cf40c67458e64e64821c025f8d2a062
dwnorm2 <dwnorm2@gmail.com>
2024-03-04 21:10:39 -0600

    format

--- main.js
+++ main.js
@@ -4163,10 +4163,10 @@ function factorial(n) {
 // ENJOY CODING :)
 
 function sumOfMinimums(arr) {
-  let sum = 0
+  let sum = 0;
   for (let i = 0; i < arr.length; i++) {
-    sum += Math.min(...arr[i])
+    sum += Math.min(...arr[i]);
   }
 
-  return sum
+  return sum;
 }
