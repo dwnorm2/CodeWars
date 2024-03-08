fc15d68a578b5b15d04ee2fa49c0055fa87aa9d3
dwnorm2 <dwnorm2@gmail.com>
2024-03-08 12:32:13 -0600

    1 kata

--- main.js
+++ main.js
@@ -4254,3 +4254,16 @@ function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
     ) / 2
   );
 }
+
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+// Example:
+
+// "riley" --> "Hello Riley!"
+// "JACK"  --> "Hello Jack!"
+
+var greet = function (name) {
+  return `Hello ${name.slice(0, 1).toUpperCase()}${name
+    .slice(1, name.length)
+    .toLowerCase()}!`;
+};
