1fbda7298d045168ef76327b0ac98387b1be675b
dwnorm2 <dwnorm2@gmail.com>
2024-03-11 12:46:36 -0500

    1 kata

--- main.js
+++ main.js
@@ -4330,3 +4330,23 @@ function sumCubes(n) {
 
   return sum;
 }
+
+// Complete the method which accepts an array of integers, and returns one of the following:
+
+//     "yes, ascending" - if the numbers in the array are sorted in an ascending order
+//     "yes, descending" - if the numbers in the array are sorted in a descending order
+//     "no" - otherwise
+
+// You can assume the array will always be valid, and there will always be one correct answer.
+
+function isSortedAndHow(array) {
+  let sorted = array.slice().sort((a, b) => a - b);
+  let descSorted = array.slice().sort((a, b) => b - a);
+  if (array.join("") == sorted.join("")) {
+    return "yes, ascending";
+  } else if (array.join("") == descSorted.join("")) {
+    return "yes, descending";
+  } else {
+    return "no";
+  }
+}
