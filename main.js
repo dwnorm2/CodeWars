8d2f8b8484ea3d15099c1a2b3f62155b215079a2
dwnorm2 <dwnorm2@gmail.com>
2024-03-13 20:36:07 -0500

    1 kata

--- main.js
+++ main.js
@@ -4356,3 +4356,18 @@ function isSortedAndHow(array) {
 function include(arr, item) {
   return arr.includes(item);
 }
+
+// Unfinished Loop - Bug Fixing #1
+
+// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+function createArray(number){
+  var newArray = [];
+  
+  for(var counter = 1; counter <= number;){
+    newArray.push(counter);
+    counter++
+  }
+  
+  return newArray;
+}
