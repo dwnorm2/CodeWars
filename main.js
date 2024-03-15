1de9cef012b8fc9751fc81b87251b96a0ad6460b
dwnorm2 <dwnorm2@gmail.com>
2024-03-15 12:12:15 -0500

    1 kata

--- main.js
+++ main.js
@@ -4387,3 +4387,19 @@ function solution(a, b) {
   let arr = [a, b].sort((a, b) => a.length - b.length);
   return arr[0] + arr[1] + arr[0];
 }
+
+// Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
+
+// Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as intended (i.e. it must make the first character in the string upper case).
+
+// The string will always start with a letter and will never be empty.
+
+// Examples:
+
+// "hello" --> "Hello"
+// "Hello" --> "Hello" (the first letter was already capitalized)
+// "a"     --> "A"
+
+function capitalizeWord(word) {
+  return word[0].toUpperCase() + word.slice(1);
+}
