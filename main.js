bd0dcb31e435ab0feb69da7af0246daf7d860376
dwnorm2 <dwnorm2@gmail.com>
2024-03-19 16:44:37 -0500

    1 kata

--- main.js
+++ main.js
@@ -4536,3 +4536,32 @@ function sameCase(a, b) {
 function squareOrSquareRoot(array) {
   return array.map((n) => (Math.sqrt(n) % 1 == 0 ? Math.sqrt(n) : n ** 2));
 }
+
+// In javascript, Object is one of basic data types. Define an Object can use var obj=new Object() or var obj={}
+
+// You can define the object attributes during initialization, like this:
+
+// var animal={name:"dog"}
+
+// you can also set/get some properties after the object definition, like this:
+
+// var animal={}
+// animal.name="dog"  (or animal["name"]="dog")
+
+// Task
+
+// Give you a function animal, accept 1 parameter:obj like this:
+
+// {name:"dog",legs:4,color:"white"}
+
+// and return a string like this:
+
+// "This white dog has 4 legs."
+
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+// In the end, click "Submit" to submit your code pass this kata.
+
+function animal(obj) {
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
