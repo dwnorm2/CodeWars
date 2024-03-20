f393c0387d5978752d4ca7f5467900f2b98819ed
dwnorm2 <dwnorm2@gmail.com>
2024-03-20 11:16:17 -0500

    refactor

--- main.js
+++ main.js
@@ -4576,5 +4576,5 @@ function animal(obj) {
 // [-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
 
 function multipleOfIndex(array) {
-  return array.filter((n, i) => n % i == 0 || (n == 0 && i == 0));
+  return array.filter((n, i) => n % i == 0 || n == 0);
 }
