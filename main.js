cffc2860a50cf9d22e06f265d2ff48bd951f46c8
dwnorm2 <dwnorm2@gmail.com>
2024-03-20 11:15:17 -0500

    1 kata

--- main.js
+++ main.js
@@ -4565,3 +4565,16 @@ function squareOrSquareRoot(array) {
 function animal(obj) {
   return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
 }
+
+// Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+// Some cases:
+
+// [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+
+// [68, -1, 1, -7, 10, 10] => [-1, 10]
+
+// [-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
+
+function multipleOfIndex(array) {
+  return array.filter((n, i) => n % i == 0 || (n == 0 && i == 0));
+}
