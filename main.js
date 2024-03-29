1a66ce948f17d8922cb955babe405ec83b4b6e5d
dwnorm2 <dwnorm2@gmail.com>
2024-03-29 14:24:01 -0500

    1 kata

--- main.js
+++ main.js
@@ -4715,3 +4715,11 @@ function mystery() {
 function seatsInTheater(nCols, nRows, col, row) {
   return (nCols - (col - 1)) * (nRows - row);
 }
+
+// I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+// Can you figure out what's wrong here?
+
+function swapValues() {
+  arguments[0].reverse();
+}
