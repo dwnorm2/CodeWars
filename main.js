f086040d83a07008c4faebcd8ff344207351d37e
dwnorm2 <dwnorm2@gmail.com>
2024-04-01 11:05:29 -0500

    1 kata

--- main.js
+++ main.js
@@ -4768,3 +4768,27 @@ Sleigh.prototype.authenticate = function (name, password) {
 function derive(coefficient, exponent) {
   return `${coefficient * exponent}x^${exponent - 1}`;
 }
+
+// Your task is to find the nearest square number, nearest_sq(n) or nearestSq(n), of a positive integer n.
+
+// For example, if n = 111, then nearest\_sq(n) (nearestSq(n)) equals 121, since 111 is closer to 121, the square of 11, than 100, the square of 10.
+
+// If the n is already the perfect square (e.g. n = 144, n = 81, etc.), you need to just return n.
+
+function nearestSq(n) {
+  if (Math.sqrt(n) % 1 == 0) {
+    return n;
+  }
+  let higherSquare;
+  let lowerSquare;
+
+  for (let i = n; Math.sqrt(i) % 1 !== 0; i++) {
+    higherSquare = i + 1;
+  }
+
+  for (let i = n; Math.sqrt(i) % 1 !== 0; i--) {
+    lowerSquare = i - 1;
+  }
+
+  return higherSquare - n < n - lowerSquare ? higherSquare : lowerSquare;
+}
