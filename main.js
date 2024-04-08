69ff5cec7ec238f17943b170ab47bb5c3b2e8d09
dwnorm2 <dwnorm2@gmail.com>
2024-04-08 17:47:07 -0500

    1 kata

--- main.js
+++ main.js
@@ -4928,3 +4928,19 @@ function popElement(arr) {
 function buildString(...template) {
   return `I like ${template.join(", ")}!`;
 }
+
+// Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+
+// E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+
+// Note: You can expect all of the inputs to be the same length.
+
+function tripleTrouble(one, two, three) {
+  let word = "";
+  for (let i = 0; i < one.length; i++) {
+    word += one[i];
+    word += two[i];
+    word += three[i];
+  }
+  return word;
+}
