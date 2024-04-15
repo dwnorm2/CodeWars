9c6e442651d1bce6c027a21a58b475b4260df691
dwnorm2 <dwnorm2@gmail.com>
2024-04-15 12:06:01 -0500

    1 kata

--- main.js
+++ main.js
@@ -5072,3 +5072,20 @@ function stairsIn20(s) {
 function stairsIn20(s) {
   return s.flat().reduce((total, s) => total + s, 0) * 20;
 }
+
+// Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+
+//   3 -->    15  (  3 * 5¹)
+//  10 -->   250  ( 10 * 5²)
+// 200 --> 25000  (200 * 5³)
+//   0 -->     0  (  0 * 5¹)
+//  -3 -->   -15  ( -3 * 5¹)
+
+let multiply = (number) => {
+  return (
+    number *
+    (number > 0
+      ? 5 ** number.toString().length
+      : 5 ** (number.toString().length - 1))
+  );
+};
