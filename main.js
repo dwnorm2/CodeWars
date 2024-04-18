b561818de3352d8fb4b223f658d0a5b90ba1c90a
dwnorm2 <dwnorm2@gmail.com>
2024-04-18 11:13:04 -0500

    1 kata

--- main.js
+++ main.js
@@ -5140,3 +5140,18 @@ function defineSuit(card) {
     return "spades";
   }
 }
+
+// Sometimes, I want to quickly be able to convert miles per imperial gallon (mpg) into kilometers per liter (kpl).
+
+// Create an application that will display the number of kilometers per liter (output) based on the number of miles per imperial gallon (input).
+
+// Make sure to round off the result to two decimal points.
+
+// Some useful associations relevant to this kata:
+
+//     1 Imperial Gallon = 4.54609188 litres
+//     1 Mile = 1.609344 kilometres
+
+function converter(mpg) {
+  return Math.round(100 * (mpg * (1.609344 / 4.54609188))) / 100;
+}
