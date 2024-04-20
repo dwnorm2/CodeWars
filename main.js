1891645ee43bb462a318d4dd15d7236f2f487a8e
dwnorm2 <dwnorm2@gmail.com>
2024-04-20 18:12:53 -0500

    1 kata

--- main.js
+++ main.js
@@ -5176,3 +5176,39 @@ function twoOldestAges(ages) {
   let sorted = ages.sort((a, b) => a - b);
   return sorted.slice(-2);
 }
+
+// In the following 6 digit number:
+
+// 283910
+
+// 91 is the greatest sequence of 2 consecutive digits.
+
+// In the following 10 digit number:
+
+// 1234567890
+
+// 67890 is the greatest sequence of 5 consecutive digits.
+
+// Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits.
+
+// Adapted from ProjectEuler.net
+
+function solution(digits) {
+  let largest = 0;
+  let arr = digits.toString().split("");
+  for (let i = 0; i < arr.length - 4; i++) {
+    if (
+      `${arr[i]}${arr[i + 1]}${arr[i + 2]}${arr[i + 3]}${arr[i + 4]}` > largest
+    )
+      largest = `${arr[i]}${arr[i + 1]}${arr[i + 2]}${arr[i + 3]}${arr[i + 4]}`;
+  }
+  return +largest;
+}
+
+function solution(digits) {
+  let largest = 0;
+  for (let i = 0; i < digits.toString().length - 4; i++) {
+    if (digits.slice(i, i + 5) > largest) largest = digits.slice(i, i + 5);
+  }
+  return +largest;
+}
