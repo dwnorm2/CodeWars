e136fcb1b49bcc8132bba83c9db39c58ef80a5f3
dwnorm2 <dwnorm2@gmail.com>
2024-04-27 19:23:06 -0500

    1 kata

--- main.js
+++ main.js
@@ -5292,3 +5292,11 @@ function elevator(left, right, call){
     return "right"
   }
 }
+
+// You task to pass only this tests :
+
+//     a == false
+//     !a == false
+//     a == !a
+
+const a = "0"
