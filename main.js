89cd4442fe81437b82391c1e1e5d8f72421551df
dwnorm2 <dwnorm2@gmail.com>
2024-04-29 22:16:25 -0500

    1 kata

--- main.js
+++ main.js
@@ -5317,3 +5317,15 @@ function sakuraFall(v) {
   //branch is 400cm high
   return v > 0 ? 400 / v : 0
 }
+
+// Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+// E.g.
+
+// arrayMadness([4, 5, 6], [1, 2, 3]); // returns true since 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+
+// Get your timer out. Are you ready? Ready, get set, GO!!!
+
+function arrayMadness(a, b) {
+  return a.reduce((total,x) => total + x**2, 0) > b.reduce((total,y) => total + y**3, 0)
+}
