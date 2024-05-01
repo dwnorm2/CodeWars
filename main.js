39e4c9c7775fbe8d4267a75a1c2c82a19b289050
dwnorm2 <dwnorm2@gmail.com>
2024-04-30 19:07:36 -0500

    1 kata

--- main.js
+++ main.js
@@ -5329,3 +5329,15 @@ function sakuraFall(v) {
 function arrayMadness(a, b) {
   return a.reduce((total,x) => total + x**2, 0) > b.reduce((total,y) => total + y**3, 0)
 }
+
+// *** No Loops Allowed ***
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value, without using a loop.
+
+// Array can contain numbers or strings. x can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+// Looking for more, loop-restrained fun? Check out the other kata in the series:
+
+function check(a,x){
+  return a.includes(x)
+};
