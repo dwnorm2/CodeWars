d3a32c6648cb4d4b6ff454662fb26386acf35397
dwnorm2 <dwnorm2@gmail.com>
2024-05-03 18:03:57 -0500

    1 kata

--- main.js
+++ main.js
@@ -5377,3 +5377,11 @@ function remove(string) {
   }
   return string;
 }
+
+// Get ASCII value of a character.
+
+// For the ASCII table you can refer to http://www.asciitable.com/
+
+function getASCII(c) {
+  return c.charCodeAt(0);
+}
