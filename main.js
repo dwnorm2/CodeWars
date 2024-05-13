942efb58ad584a91982d0e374d437fb950406217
dwnorm2 <dwnorm2@gmail.com>
2024-05-12 20:32:46 -0500

    1 kata

--- main.js
+++ main.js
@@ -5531,3 +5531,16 @@ function nextId(ids) {
 function odds(values) {
   return values.filter((n) => n % 2 !== 0);
 }
+
+// You will be given an array of numbers which can be used using the String.fromCharCode() (JS), Tools.FromCharCode() (C#) method to convert the number to a character. It is recommended to map over the array of numbers and convert each number to the corresponding ascii character.
+// Examples
+
+// These are example of how to convert a number to an ascii Character:
+// Javascript => String.fromCharCode(97) // a
+// C# => Tools.FromCharCode(97) // a
+
+// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+var ArrowFunc = function (arr) {
+  return arr.map((x) => String.fromCharCode(x)).join(""); //Complete this function
+};
