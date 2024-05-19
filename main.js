53e436d2d5dfd08947ebfbd6829e161a82527ff5
dwnorm2 <dwnorm2@gmail.com>
2024-05-18 20:23:47 -0500

    1 kata

--- main.js
+++ main.js
@@ -5620,3 +5620,18 @@ function sixToast(num) {
     return num - 6;
   }
 }
+
+// Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+// Function:
+
+// getNumberFromString(s)
+
+function getNumberFromString(s) {
+  return Number(
+    s
+      .split("")
+      .filter((char) => !isNaN(parseInt(char)))
+      .join("")
+  );
+}
