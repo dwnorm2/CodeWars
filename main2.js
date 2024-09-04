d0cc813e077cec0cf01632b6c3e26441bceab306
dwnorm2 <dwnorm2@gmail.com>
2024-09-03 19:23:31 -0500

    Your commit message

--- main2.js
+++ main2.js
@@ -284,3 +284,24 @@ function descendingOrder(n) {
     .sort((a, b) => b - a)
     .join("");
 }
+
+// Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// Examples:
+
+// "Hey fellow warriors"  --> "Hey wollef sroirraw"
+// "This is a test        --> "This is a test"
+// "This is another test" --> "This is rehtona test"
+
+function spinWords(string) {
+  function toReverse(x) {
+    if (x.length >= 5) {
+      return x.split("").reverse().join("");
+    }
+    return x;
+  }
+  return string
+    .split(" ")
+    .map((x) => toReverse(x))
+    .join(" ");
+}
