aa65094ba8bc26206bc62ffec5e1bd113a25ed07
dwnorm2 <dwnorm2@gmail.com>
2024-09-05 13:11:38 -0500

    1 kata

--- main2.js
+++ main2.js
@@ -5,3 +5,7 @@
 function multiply(a, b) {
   return a * b;
 }
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+let evenOrOdd = (number) => (number % 2 == 0 ? "Even" : "Odd");
