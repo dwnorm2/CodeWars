f7c5596e09186ef996d4a7c18bda7e0dcd316426
dwnorm2 <dwnorm2@gmail.com>
2024-09-06 18:03:28 -0500

    1 kata

--- main2.js
+++ main2.js
@@ -9,3 +9,16 @@ function multiply(a, b) {
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 let evenOrOdd = (number) => (number % 2 == 0 ? "Even" : "Odd");
+
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+// Examples (input --> output):
+
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+function numberToString(num) {
+  return num.toString();
+}
