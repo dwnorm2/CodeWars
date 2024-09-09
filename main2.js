54230110c556c19218a81ff72ce803183d6ae597
dwnorm2 <dwnorm2@gmail.com>
2024-09-09 11:09:12 -0500

    1 kata

--- main2.js
+++ main2.js
@@ -105,3 +105,15 @@ function disemvowel(str) {
 function solution(str) {
   return str.split("").reverse().join("");
 }
+
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+  return -number;
+}
