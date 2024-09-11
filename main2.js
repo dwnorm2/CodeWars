7ba5e1d6a395bab2814bb83521178e097bbce3ea
dwnorm2 <dwnorm2@gmail.com>
2024-09-10 22:08:36 -0500

    1 kata

--- main2.js
+++ main2.js
@@ -150,3 +150,22 @@ function likes(names) {
 function boolToWord(bool) {
   return bool ? "Yes" : "No";
 }
+
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+// Example
+
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+
+// The returned format must be correct in order to complete this challenge.
+
+// Don't forget the space after the closing parentheses!
+
+function createPhoneNumber(numbers) {
+  let number = "(xxx) xxx-xxxx";
+
+  for (let i = 0; i < numbers.length; i++) {
+    number = number.replace("x", numbers[i]);
+  }
+
+  return number;
+}
