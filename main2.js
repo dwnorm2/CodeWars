34bb5bc77777eb1ea365acec0634c254bc7baa74
dwnorm2 <dwnorm2@gmail.com>
2024-09-11 15:37:25 -0500

    1 kata

--- main2.js
+++ main2.js
@@ -186,3 +186,13 @@ function squareDigits(num) {
     .map((n) => n ** 2)
     .join("");
 }
+
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+  return arr.reduce((x, y) => x + (y > 0 ? y : 0), 0);
+}
