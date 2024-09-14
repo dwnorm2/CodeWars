590bbbe84d085cccc5bced0d2e5eb7562196ba24
dwnorm2 <dwnorm2@gmail.com>
2024-09-14 12:40:22 -0500

    1 kata

--- main2.js
+++ main2.js
@@ -226,3 +226,9 @@ function highAndLow(numbers) {
   let arr = numbers.split(" ");
   return `${Math.max(...arr)} ${Math.min(...arr)}`;
 }
+
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry about strings with less than two characters.
+
+function removeChar(str) {
+  return str.slice(1, -1);
+}
