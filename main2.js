366c074566c72fd12cddb79af22c02ebcb6e494a
dwnorm2 <dwnorm2@gmail.com>
2024-11-26 17:09:54 -0600

    1 kata

--- main2.js
+++ main2.js
@@ -310,3 +310,16 @@ function spinWords(string) {
 var isSquare = function (n) {
   return Math.sqrt(n) % 1 == 0 ? true : false;
 };
+
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+//     Given [34, 15, 88, 2] your solution will return 2
+//     Given [34, -345, -1, 100] your solution will return -345
+
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+function findSmallestInt(arr) {
+  return Math.min(...arr)
+};
