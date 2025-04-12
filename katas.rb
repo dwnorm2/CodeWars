927e5d93a79b8bac5855fc31636871863d8c959c
dwnorm2 <dwnorm2@gmail.com>
2025-04-12 18:59:36 -0500

    1 kata

--- katas.rb
+++ katas.rb
@@ -63,3 +63,8 @@ def make_negative(num)
   return -num.abs
 end
 
+# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+ 
+def bool_to_word bool
+return bool ? "Yes" : "No"
+end
