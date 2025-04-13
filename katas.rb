ce058ef5b3d7055e29893f907ce0ea56ec76f171
dwnorm2 <dwnorm2@gmail.com>
2025-04-12 19:09:44 -0500

    1 kata

--- katas.rb
+++ katas.rb
@@ -68,3 +68,28 @@ end
 def bool_to_word bool
 return bool ? "Yes" : "No"
 end
+
+# Task
+# You get an array of numbers, return the sum of all of the positives ones.
+
+# Example
+# [1, -4, 7, 12] => 
+# 1
+# +
+# 7
+# +
+# 12
+# =
+# 20
+# 1+7+12=20
+# Note
+# If there is nothing to sum, the sum is default to 0.
+
+def positive_sum(arr)
+  total = 0
+  arr.each do |num|
+    num > 0 ? total += num : total+=0
+  end
+  total
+end
+
