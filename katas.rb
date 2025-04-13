1404d591e3753b29eaf6cc56f66aab6c39f4fea7
dwnorm2 <dwnorm2@gmail.com>
2025-04-12 19:29:43 -0500

    1 kata

--- katas.rb
+++ katas.rb
@@ -93,3 +93,12 @@ def positive_sum(arr)
   total
 end
 
+# Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+# Examples (input -> output)
+# 6, "I"     -> "IIIIII"
+# 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+def repeat_str (n, s)
+  s*n
+end
