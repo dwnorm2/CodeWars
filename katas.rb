4b3baf241238b637ba036f018c7d623e534dbfbf
dwnorm2 <dwnorm2@gmail.com>
2025-04-16 13:23:18 -0500

    1 kata

--- katas.rb
+++ katas.rb
@@ -159,3 +159,23 @@ end
 def string_to_number(s)
 s.to_i
 end
+
+# Summation
+# Write a program that finds the summation of every number from 1 to num (both inclusive). The number will always be a positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the sample tests.
+
+# For example (Input -> Output):
+
+# 2 -> 3 (1 + 2)
+# 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+def summation(num)
+  sum = 0
+  for i in 1..num do
+    sum += i
+  end
+  sum
+end
+
+def summation(num)
+  (1..num).sum
+end
