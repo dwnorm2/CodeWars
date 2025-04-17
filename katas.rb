afcedee2f74797b3e8691a0aab5830eeec8471fc
dwnorm2 <dwnorm2@gmail.com>
2025-04-17 13:27:16 -0500

    1 kata

--- katas.rb
+++ katas.rb
@@ -179,3 +179,12 @@ end
 def summation(num)
   (1..num).sum
 end
+
+# Make a simple function called greet that returns the most-famous "hello world!".
+
+# Style Points
+# Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+def greet
+  "hello world!".split("").join("")
+end
