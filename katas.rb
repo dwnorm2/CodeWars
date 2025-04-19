174981927ef3ae7abf8cca31340d839a3657334e
dwnorm2 <dwnorm2@gmail.com>
2025-04-18 21:55:49 -0500

    1 kata

--- katas.rb
+++ katas.rb
@@ -252,3 +252,18 @@ end
 def greet(name)
   "Hello, #{name} how are you doing today?"
 end
+
+# Your task is to create a function that does four basic mathematical operations.
+
+# The function should take three arguments - operation(string/char), value1(number), value2(number).
+# The function should return result of numbers after applying the chosen operation.
+
+# Examples(Operator, value1, value2) --> output
+# ('+', 4, 7) --> 11
+# ('-', 15, 18) --> -3
+# ('*', 5, 5) --> 25
+# ('/', 49, 7) --> 7
+
+def basic_op(operator, value1, value2)
+  eval("#{value1} #{operator} #{value2}")
+end
