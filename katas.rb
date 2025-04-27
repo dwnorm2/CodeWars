87fb1571ed17bad165da540cfab8f9629a29008a
dwnorm2 <dwnorm2@gmail.com>
2025-04-27 13:38:33 -0500

    1 kata

--- katas.rb
+++ katas.rb
@@ -422,3 +422,9 @@ end
 def are_you_playing_banjo(name)
   name[0].upcase == "R" ? "#{name} plays banjo" : "#{name} does not play banjo"
 end
+
+# This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+def simple_multiplication(number)
+  number % 2 == 0 ? number*8 : number*9
+end
