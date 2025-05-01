51ab502f5b06a969772d0ad992a093a91ad7bb1c
dwnorm2 <dwnorm2@gmail.com>
2025-04-30 23:23:10 -0500

    1 kata

--- katas.rb
+++ katas.rb
@@ -462,3 +462,14 @@ end
 def find_needle(haystack)
   "found the needle at position #{haystack.index("needle")}"
 end
+
+# Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+# [1, 2, 3, 4, 5] --> [-1, -2, -3, -4, -5]
+# [1, -2, 3, -4, 5] --> [-1, 2, -3, 4, -5]
+# [] --> []
+# You can assume that all values are integers. Do not mutate the input array.
+
+def invert(list)
+  list.map{|n| n*-1}
+end
