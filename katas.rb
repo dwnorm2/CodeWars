cf846bc6df587ccbc6fdf514eee9408730175d61
dwnorm2 <dwnorm2@gmail.com>
2025-05-03 14:25:44 -0500

    1 kata

--- katas.rb
+++ katas.rb
@@ -497,3 +497,11 @@ end
 def smash(words)
   words.join(" ")
 end
+
+# Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+# [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+def grow(x) 
+  x.inject(:*)
+end
