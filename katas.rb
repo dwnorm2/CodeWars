7d003f49115347eaf6f86d383f79567bbf4ded01
dwnorm2 <dwnorm2@gmail.com>
2025-05-05 23:20:32 -0500

    1 kata

--- katas.rb
+++ katas.rb
@@ -525,3 +525,11 @@ end
 def better_than_average(arr, points)
   (arr.sum + points) / (arr.length + 1) < points
 end
+
+# A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+# Return true if yes, false otherwise :)
+
+def hero(bullets, dragons)
+  bullets >= dragons*2
+end
