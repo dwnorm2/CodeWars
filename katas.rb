b6ef17b1a33bbf9a6e3660377cee28cbd8dbc0e7
dwnorm2 <dwnorm2@gmail.com>
2025-05-07 21:22:15 -0500

    1 kata

--- katas.rb
+++ katas.rb
@@ -546,3 +546,18 @@ end
 def count_positives_sum_negatives(lst)
   lst.size == 0 ? [] : [lst.select { |x| x > 0}.length, lst.select { |x| x <0}.sum]
 end
+
+# Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+# Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+# Create a function which translates a given DNA string into RNA.
+
+# For example:
+
+# "GCAT"  =>  "GCAU"
+# The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+def dna_to_rna(dna)
+  dna.gsub("T","U")
+end
