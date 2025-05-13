f15377a3bd984073d4c8fbebbb20755d56809055
dwnorm2 <dwnorm2@gmail.com>
2025-05-12 19:46:23 -0500

    1 kata

--- katas.rb
+++ katas.rb
@@ -572,4 +572,17 @@ def zero_fuel(distance, mpg, fuel_left)
   distance/mpg.to_f <= fuel_left
 end
 
-# test
+# Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+# When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+# Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+# S is misinterpreted as 5
+# O is misinterpreted as 0
+# I is misinterpreted as 1
+# The test cases contain numbers only by mistake.
+
+def correct(string)
+  string.gsub("0", "O").gsub("1", "I").gsub("5", "S")
+end
