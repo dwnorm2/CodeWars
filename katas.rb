14ac045d82f91ef26095f70b6ed16b86c05dbe1b
dwnorm2 <dwnorm2@gmail.com>
2025-05-14 17:55:56 -0500

    1 kata

--- katas.rb
+++ katas.rb
@@ -607,3 +607,11 @@ end
 def _if(bool, ifTrue, ifFalse)
   bool ? ifTrue.call : ifFalse.call  
 end 
+
+# Bob needs a fast way to calculate the volume of a rectangular cuboid with three values: the length, width and height of the cuboid.
+
+# Write a function to help Bob with this calculation.
+
+def get_volume_of_cuboid(length, width, height)
+    length*width*height
+end
