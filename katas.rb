d1fd7c3e0d16ca4641757bfcefd7e6e0ccf969cd
dwnorm2 <dwnorm2@gmail.com>
2025-05-14 13:00:59 -0500

    1 kata

--- katas.rb
+++ katas.rb
@@ -595,3 +595,15 @@ end
 def solution(str)
   str.reverse
 end
+
+# Create a function called _if which takes 3 arguments: a value bool and 2 functions (which do not take any parameters): func1 and func2
+
+# When bool is truthy, func1 should be called, otherwise call the func2.
+
+# Example:
+# _if(true, proc{puts "True"}, proc{puts "False"})
+# # Logs 'True' to the console.
+ 
+def _if(bool, ifTrue, ifFalse)
+  bool ? ifTrue.call : ifFalse.call  
+end 
