8b7ebe203c717031411b1ab2e47db010bc8811c1
dwnorm2 <dwnorm2@gmail.com>
2025-05-18 11:48:34 -0500

    1 kata

--- katas.rb
+++ katas.rb
@@ -615,3 +615,27 @@ end
 def get_volume_of_cuboid(length, width, height)
     length*width*height
 end
+
+# Complete the function which returns the weekday according to the input number:
+
+# 1 returns "Sunday"
+# 2 returns "Monday"
+# 3 returns "Tuesday"
+# 4 returns "Wednesday"
+# 5 returns "Thursday"
+# 6 returns "Friday"
+# 7 returns "Saturday"
+# Otherwise returns "Wrong, please enter a number between 1 and 7"
+
+def what_day?(n)
+  days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ]
+  return n < 1 || n > 7 ? "Wrong, please enter a number between 1 and 7" : days[n - 1]
+end
