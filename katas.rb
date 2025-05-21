efbb6e10fb4ad702e783a4ea05abfe5e9e52d83a
dwnorm2 <dwnorm2@gmail.com>
2025-05-20 22:18:52 -0500

    1 kata

--- katas.rb
+++ katas.rb
@@ -639,3 +639,16 @@ def what_day?(n)
   ]
   return n < 1 || n > 7 ? "Wrong, please enter a number between 1 and 7" : days[n - 1]
 end
+
+# The male gametes or sperm cells in humans and other mammals are heterogametic and contain one of two types of sex chromosomes. They are either X or Y. The female gametes or eggs however, contain only the X sex chromosome and are homogametic.
+
+# The sperm cell determines the sex of an individual in this case. If a sperm cell containing an X chromosome fertilizes an egg, the resulting zygote will be XX or female. If the sperm cell contains a Y chromosome, then the resulting zygote will be XY or male.
+
+# Determine if the sex of the offspring will be male or female based on the X or Y chromosome present in the male's sperm.
+
+# If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter."; If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
+
+def chromosome_check(sperm)
+ sperm.include?("Y") ? "Congratulations! You're going to have a son."
+  : "Congratulations! You're going to have a daughter."
+end
